f5749027720ca313ea37ff2ae23c3ff47c21e13c
LeeJR <ljr4488@naver.com>
2022-10-12 09:18:53 +0900

    첫번째 수정

---
+++ src/main/java/com/example/people/controller/PeopleController.java
@@ -0,0 +1,12 @@
+package com.example.people.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PeopleController {
+    @GetMapping(value = "/api/Hello")
+    public String HelloWorld() {
+        return "HelloWorld";
+    }
+}
